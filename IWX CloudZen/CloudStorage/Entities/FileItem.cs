bcd78224df160a847f91813b744dfb03dc75dc3c
manojrajgopal <manojrajgopal15@gmail.com>
2026-03-22 09:36:19 +0530

    Cloud Storage file structure is created

---
+++ IWX CloudZen/CloudStorage/Entities/FileItem.cs
@@ -0,0 +1,15 @@
+﻿namespace IWX_CloudZen.CloudStorage.Entities
+{
+    public class FileItem
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FileUrl { get; set; }
+        public string StorageProvider { get; set; }
+        public string ContentType { get; set; }
+        public long Size { get; set; }
+        public string UploadedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Folder { get; set; }
+    }
+}
